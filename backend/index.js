e28c66343c5e443ec70fb8737e49e973b687bf0a
Nethu00 <nethunirajapakse@gmail.com>
2024-09-05 23:32:32 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const socketIo = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS for specific origin (customize as needed)
 app.use(
