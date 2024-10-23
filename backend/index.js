0ca7146ff1b36a18cd60a1e0c688e0196948d649
Nethu00 <nethunirajapakse@gmail.com>
2024-10-23 23:57:00 +0530

    Added dependencies

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,11 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Server is running on port 3000!");
+  res.send("Server is running on port 3000!"); //sending plain text
 });
 
 app.get("/message", (req, res) => {
-  res.json({ message: "Server is running on port 3000" });
+  res.json({ message: "Server is running on port 3000.." }); //sending JSON-encoded data.
 });
 
 // Error handling middleware
@@ -46,3 +46,29 @@ app.use((err, req, res, next) => {
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const URI =
+  "mongodb+srv://nethunirajapakse:<nethunirajapakse>@cluster1.0hr7h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
